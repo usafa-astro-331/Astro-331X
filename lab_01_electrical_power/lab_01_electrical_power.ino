b16266a4b1c8dc0af046425b94822313e4c03078
jordanfirthussf <jordan.firth@afacademy.af.edu>
2024-05-08 07:58:03 -0600

    update from 2024 spring electrical lab

--- lab_01_electrical_power/lab_01_electrical_power.ino
+++ lab_01_electrical_power/lab_01_electrical_power.ino
@@ -1,30 +1,28 @@
-#include <LiquidCrystal.h>
-// initialize the library by associating any needed LCD interface pin
-// with the arduino pin number it is connected to
-const int rs = 5, en = 4, d4 = 1, d5 = 0, d6 = A6, d7 = A5;
-LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
-
-
 #include <Adafruit_INA219.h>
 Adafruit_INA219 ina219;
 
 #include <SPI.h>
 #include <SD.h>
 
-const int chipSelect = SDCARD_SS_PIN;
+#ifdef ARDUINO_TEENSY41 
+  const int chipSelect = BUILTIN_SDCARD;
+#else // Arduino MKR Zero
+  const int chipSelect = SDCARD_SS_PIN;
+#endif
 
 
 void setup() {
-  lcd.begin(16,2); // size of parallel lcd is 16x2 characters
+  delay(1000);
     
   Serial.begin(9600);
   // while(!Serial);
   Serial.println("test");
+
   // Initialize the INA219.
   // By default the initialization will use the largest range (32V, 2A).  However
   // you can call a setCalibration function to change this range (see comments).
   if (! ina219.begin()) {
-    Serial.println("Failed to find INA219 chip");
+    Serial.println("Failed to find INA219");
     while (1) { delay(10); }
   }
   // To use a slightly lower 32V, 1A range (higher precision on amps):
@@ -32,14 +30,13 @@ void setup() {
   // Or to use a lower 16V, 400mA range (higher precision on volts and amps):
   //ina219.setCalibration_16V_400mA();
 
-  Serial.println("           INA219:  ");
-  Serial.println("time (ms), curr (mA)");
-
+  Serial.println("time (ms), current (mA), voltage (V)");
 
     Serial.print("Initializing SD card...");
   // see if the card is present and can be initialized:
   if (!SD.begin(chipSelect)) {
     Serial.println("Card failed, or not present");
+  
     // don't do anything more:
     while (1);
   }
@@ -52,7 +49,7 @@ void setup() {
                 dataFile.close();
               }
               // if the file isn't open, pop up an error:
-              else { Serial.println("error opening datalog.txt");  }
+              else { Serial.println("error opening log file");  }
 
 } // end function setup()
 
@@ -65,7 +62,7 @@ struct electrical_measurement
 // prototype function for INA219 reading
 electrical_measurement INA219reading() ;
 
-  int interval = 500; // writes measurements every XX ms
+  int interval = 200; // writes measurements every XX ms
   int present = millis(); 
   int due = present += interval; 
 
@@ -90,7 +87,7 @@ if (averaging_index >= num_samples){
 
   present = millis(); 
   if (present >= due){
-    String write_line = "";
+    String write_line = "time:";
     write_line += present; 
     
     // INA219
@@ -103,32 +100,25 @@ if (averaging_index >= num_samples){
     
     current = current / float(num_samples); 
     voltage = voltage / float(num_samples);
-    write_line += ", ";
+    write_line += ", current:";
     write_line += current; 
-    write_line += ", ";
+    write_line += ", voltage:";
     write_line += voltage;
     
-    lcd.setCursor(0,0);
-    lcd.print(current,1); 
-    lcd.setCursor(5,0);
-    lcd.print("mA");
-    lcd.setCursor(0,1);
-    lcd.print(voltage); 
-    lcd.setCursor(6,1);
-    lcd.print("V");
-
     File dataFile = SD.open("iv_curve.csv", FILE_WRITE);
               // if the file is available, write to it:
               if (dataFile) {
                 dataFile.println(write_line);
                 dataFile.close();
                 // print to the serial port too:
-                Serial.println(write_line);
+                
               }
               // if the file isn't open, pop up an error:
               else {
                 Serial.println("error opening datalog.txt");
               } // end if dataFile
+
+Serial.println(write_line);
 
     due += interval; 
     
