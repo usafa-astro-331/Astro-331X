a2d655194b55f84ae23d14d2543f9e82e155ad09
jordanfirthussf <jordan.firth@afacademy.af.edu>
2024-05-08 10:19:25 -0600

    took lab 4a attitude determination from 2024 spring class

---
+++ lab_04a_attitude_determination/README.md
@@ -0,0 +1,170 @@
+# Lab 4a: attitude determination
+
+In this lab you will build and test FlatSAT's attitude determination system including sun sensors, a magnetometer, and a rate gyroscope. 
+
+Prelab report instructions: 
+[https://www.overleaf.com/read/prdzpknpdtgf](https://www.overleaf.com/read/prdzpknpdtgf)
+
+
+
+If I update the code in time, you will save attitude data to FlatSAT’s SD card storage and stream it over your XBee radio link. 
+
+
+
+
+
+## equipment
+
+- 1 laptop
+- micro USB cable 
+- string
+
+
+
+## hardware
+
+- FlatSAT
+  - components from previous labs
+    - SD card must be inserted
+  - SparkFun 9 DOF IMU
+  - reaction wheel assembly
+  - 3 cell Li-ion battery
+  - BEC
+  - wago wire connectors
+
+
+
+## documentation
+
+- IMU datasheet (SparkFun 9DoF IMU)
+
+
+
+## software
+
+- Arduino IDE
+
+- Arduino SAMD drivers (already installed)
+- ensure these Arduino libraries are installed
+
+  - SparkFun 9DoF IMU
+
+- `lab_04a_attitude_determination.ino`
+
+
+
+## setup
+
+For today’s lab, FlatSAT will be powered by a 12 V lithium ion battery. The (future) motor driver and motor are powered directly from the battery’s 12 V supply. The Arduino itself is powered via a 12 V $\rightarrow$ 5 V BEC (battery eliminator circuit). Everything else is powered by Arduino’s Vcc pin (3.3 V). 
+
+
+
+Copy the setup below, but **do not place the 3rd (middle) cell into the battery holder yet**. 
+
+And
+
+**DO not connect the battery’s 12 V output at any time during this lab. Do not even place a wire in the wago connector. **
+
+
+
+**Note:** the connection between the Arduino and the 5V rail has moved from previous labs. It’s now connected to Vin. 
+
+![attitude_bb](../../fritzing_diagrams/04_attitude_bb.svg)
+
+- Add the red 9 DOF IMU (red square) to your FlatSAT as in the diagram.
+  - use a QWIIC cable to connect via the current sensor
+- Add 4 photocell voltage dividers. The photocell is R1. 
+  - ![](../../minilabs/06_metrology/sources/Resistive_divider2.svg)
+  - Vin: 3 V
+  - Ground: ground
+  - Vout: see `sun_sensor_pins.h`
+    - use pinouts defined for Arduino MKR Zero
+    - align +x (px), +y (py), -x (nx), -y (ny) with the IMU’s magnetometer coordinate frame
+    
+
+
+
+## sensor checkout
+
+Ensure you are collecting meaningful data from FlatSAT’s sensors. 
+
+
+
+Install SD card. Connect FlatSAT via USB and upload `lab_04a_attitude_determination.ino`. Open the serial plotter. 
+
+Rotate FlatSat as you shine a light at it. 
+
+- x- and y-magnetometer data should wander up and down with orientation
+- gyroscope data should spike as you rotate
+- sun sensors should each rise when pointed toward light and fall when covered
+
+
+
+## integrate sun sensor
+
+Now you will modify the code to obtain a rough sun direction from the photocells. 
+
+Create an equation/algorithm to find the magnitude of sun’s brightness in the north/anti-north direction and the east/anti-east direction. 
+
+Uncomment the bottom lines of this code segment and add your algorithm.
+
+Comment out the “output raw sun sensor data” section. (Highlight all, `ctrl`-`/`.)
+
+Upload your modified code and test with a flashlight. 
+
+``` c++
+    // read sun sensors
+    sunpx_reading = analogRead(sunpx_pin);
+    sunpy_reading = analogRead(sunpy_pin);
+    sunnx_reading = analogRead(sunnx_pin);
+    sunny_reading = analogRead(sunny_pin);    
+    
+    // output raw sun sensor data
+    write_line += ", sunpx:"; 
+    write_line += sunpx_reading; 
+    write_line += ", sunpy:"; 
+    write_line += sunpy_reading; 
+    write_line += ", sunnx:"; 
+    write_line += sunnx_reading; 
+    write_line += ", sunny:"; 
+    write_line += sunny_reading; 
+
+    // // find sun direction
+    // north =  ; // you fill in here--remember to end line with ;
+    // east =  ; // you fill in here--remember to end line with ;
+    // sun_direction = atan2(east*1.0, -north) * RAD_TO_DEG + 180; 
+    // write_line += ", sun:"; 
+    // write_line += sun_direction; 
+```
+
+
+
+
+## attitude determination
+
+Using a string loop, hang the motor platform from the hook above your workstation. Place FlatSAT and the battery holder on the platform and secure both with tape. Insert all battery cells and connect the BEC’s 5V output to FlatSAT’s Vin pin. 
+
+**Remember: Do not connect the battery’s 12 V output to FlatSAT at any time during this lab. **
+
+Spin FlatSAT gently back and forth several times. 
+
+Let FlatSAT rotate slowly through at least 3 complete revolutions. 
+
+Spin FlatSAT quickly (at least 1 RPM) through at least 3 complete revolutions. 
+
+
+
+## Lab station cleanup
+
+- Transfer saved data to your group's storage location
+- Disconnect devices from FlatSAT and all computers
+- Replace all items at your lab station the way you found them. 
+- Have your instructor check off your lab station before you depart.
+
+
+
+## Post-lab data analysis
+
+For the next lab the magnetometer data will be an input to the spacecraft’s attitude system. To use it, you must calibrate the magnetometer data. 
+
+Use the magnetometer data you gathered to find the gain and bias of your x- and y-magnetometers. 
---
+++ lab_04a_attitude_determination/lab_04a_attitude_determination.ino
@@ -0,0 +1,192 @@
+// ----- ICM 20948 IMU
+#include <ICM_20948.h>  // Click here to get the library: http://librarymanager/All#SparkFun_ICM_20948_IMU
+
+#define SERIAL_PORT Serial
+#define WIRE_PORT Wire  // Your desired Wire port.      Used when "USE_SPI" is not defined
+// The value of the last bit of the I2C address.
+// On the SparkFun 9DoF IMU breakout the default is 1, and when the ADR jumper is closed the value becomes 0
+#define AD0_VAL 1
+
+ICM_20948_I2C myICM;
+
+#include "IMU_pins.h"
+#include "./sun_sensor_pins.h"
+
+// ----- SD card -----
+#include <SPI.h>
+#include <SD.h>
+
+#ifdef ARDUINO_TEENSY41
+const int chipSelect = BUILTIN_SDCARD;
+#else  // Arduino MKR Zero
+const int chipSelect = SDCARD_SS_PIN;
+#endif
+
+void setup() {
+  SERIAL_PORT.begin(115200);
+  //  while (!SERIAL_PORT)
+  //  {
+  //  };
+
+  // ----- ICM 20948 IMU
+  WIRE_PORT.begin();
+  WIRE_PORT.setClock(400000);
+
+  bool initialized = false;
+  while (!initialized) {
+
+    myICM.begin(WIRE_PORT, AD0_VAL);
+
+
+    SERIAL_PORT.print(F("Initialization of the sensor returned: "));
+    SERIAL_PORT.println(myICM.statusString());
+    if (myICM.status != ICM_20948_Stat_Ok) {
+      SERIAL_PORT.println("Trying again...");
+      delay(500);
+    } else {
+      initialized = true;
+    }
+  }
+
+  Serial.print("Initializing SD card...");
+  // see if the card is present and can be initialized:
+  if (!SD.begin(chipSelect)) {
+    Serial.println("Card failed, or not present");
+    // don't do anything more:
+    while (1)
+      ;
+  }
+  Serial.println("card initialized.");
+  delay(2000);
+
+  File dataFile = SD.open("04a_att.dat", FILE_WRITE);
+  // if the file is available, write to it:
+  if (dataFile) {
+    String write_line = ""; 
+    write_line += "units:\n" ;
+    write_line += "time (ms)\n"; 
+    write_line += "gyr (dps)\n"; 
+    write_line += "mag (uT)\n"; 
+    write_line += "sun detector (count)\n"; 
+    write_line += "sun angle (deg)\n";
+
+    dataFile.print(write_line);
+
+    Serial.print(write_line);
+
+    dataFile.close();
+  }
+  // if the file isn't open, pop up an error:
+  else { Serial.println("error opening log file"); 
+  }
+
+}  // end function setup
+
+
+int t;
+int t0 = millis();  // set start time right before loop
+
+// write accel data (to SD and/or serial) every `write_interval` ms
+int last_wrote = 0;
+int write_interval = 100;  // ms
+
+
+String printFormattedFloat(float val, uint8_t leading, uint8_t decimals) {
+  String write_line = "";
+  float aval = abs(val);
+  if (val < 0) {
+    write_line += "-";
+    // SERIAL_PORT.print("-");
+  } else {
+    write_line += "+";
+    // SERIAL_PORT.print(" ");
+  }
+  for (uint8_t indi = 0; indi < leading; indi++) {
+    uint32_t tenpow = 0;
+    if (indi < (leading - 1)) {
+      tenpow = 1;
+    }
+    for (uint8_t c = 0; c < (leading - 1 - indi); c++) {
+      tenpow *= 10;
+    }
+    if (aval < tenpow) {
+      write_line += "0";
+      // SERIAL_PORT.print("0");
+    } else {
+      break;
+    }
+  }
+  if (val < 0) {
+    write_line += -val;
+    write_line += decimals;
+    // SERIAL_PORT.print(-val, decimals);
+  } else {
+    write_line += val;
+    write_line += decimals;
+    // SERIAL_PORT.print(val, decimals);
+  }
+  return write_line;
+}  //end printformattedfloat()
+
+String printScaledAGMT(ICM_20948_I2C *sensor) {
+  String write_line = ", gyrz:";
+  write_line += printFormattedFloat(sensor->gyrZ(), 5, 2);
+  write_line += ", magx:";
+  write_line += printFormattedFloat(sensor->magX(), 5, 2);
+  write_line += ", magy:";
+  write_line += printFormattedFloat(sensor->magY(), 5, 2);
+
+  return write_line;
+}
+
+void loop() {
+  t = millis();
+
+  if (t - last_wrote > write_interval) {
+    String write_line = "t:";
+    write_line += t;
+
+    myICM.getAGMT();                        // The values are only updated when you call 'getAGMT'
+    write_line += printScaledAGMT(&myICM);  // This function takes into account the scale settings from when the measurement was made to calculate the values with units
+
+    // read sun sensors
+    sunpx_reading = analogRead(sunpx_pin);
+    sunpy_reading = analogRead(sunpy_pin);
+    sunnx_reading = analogRead(sunnx_pin);
+    sunny_reading = analogRead(sunny_pin);    
+    
+    // output raw sun sensor data
+    write_line += ", sunpx:"; 
+    write_line += sunpx_reading; 
+    write_line += ", sunpy:"; 
+    write_line += sunpy_reading; 
+    write_line += ", sunnx:"; 
+    write_line += sunnx_reading; 
+    write_line += ", sunny:"; 
+    write_line += sunny_reading; 
+
+    // // find sun direction
+    // north =  ; // you fill in here--remember to end line with ;
+    // east =  ; // you fill in here--remember to end line with ;
+    // sun_direction = atan2(-east*1.0, -north) * RAD_TO_DEG + 180; 
+    // write_line += ", sun:"; 
+    // write_line += sun_direction; 
+    
+    Serial.println(write_line);
+
+    File dataFile = SD.open("04a_att.dat", FILE_WRITE);
+    // if the file is available, write to it:
+    if (dataFile) {
+      dataFile.println(write_line);
+      dataFile.close();
+
+    }
+    // if the file isn't open, pop up an error:
+    else {
+      Serial.println("error opening data file");
+    }
+
+    last_wrote += write_interval;
+  }
+
+}  // end loop()
