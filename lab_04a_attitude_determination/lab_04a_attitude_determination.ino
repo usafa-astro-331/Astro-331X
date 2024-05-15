725eb6cfa3b5138c23f9e5483a7e7b5ae79469c4
jordanfirthussf <jordan.firth@afacademy.af.edu>
2024-05-15 01:57:18 -0600

    updates

--- lab_04a_attitude_determination/lab_04a_attitude_determination.ino
+++ lab_04a_attitude_determination/lab_04a_attitude_determination.ino
@@ -190,6 +190,27 @@ void loop() {
     write_line += "\t"; //tab to separate fields
 		write_line += "sun:"; 
     write_line += sun_direction; 
+
+
+// magnetometer range values used for calibration
+float x_max = 1.0; 
+float x_min = -1.0; 
+float y_max = 1.0;
+float y_min = -1.0; 
+
+float x_range = (x_max - x_min)/2; 
+float x_bias = x_max-x_range; 
+float y_range = (y_max - y_min)/2; 
+float y_bias = y_max-y_range; 
+
+float magx, magy, heading; 
+    magx =  (myICM.magX() - x_bias) /x_range; 
+    magy =  (myICM.magY() - y_bias) /y_range; 
+    float Heading = atan2(magy, -magx) +PI; 
+
+    write_line += "\t"; //tab to separate fields
+		write_line += "mag_head:"; 
+    write_line += Heading; 
     
     Serial.println(write_line);
 	Serial1.println(write_line);
